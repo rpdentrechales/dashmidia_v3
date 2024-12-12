fbd5a22387e6622d3e24d7025f8ef39bc0fff00b
rpdentrechales <rpdprocorpo@gmail.com>
2024-12-12 19:53:04 +0000

    Updating files

--- views/relatorios.py
+++ views/relatorios.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 from auxiliar.auxiliar import *
+import plotly.express as px
 
 st.set_page_config(page_title="Pró-Corpo - Relatórios", page_icon="💎",layout="wide")
 
