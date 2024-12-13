315f6d0a2d4542509477c9a1fe1426df5b8925f7
rpdentrechales <rpdprocorpo@gmail.com>
2024-12-13 14:29:51 +0000

    Updating files

--- views/relatorios.py
+++ views/relatorios.py
@@ -5,7 +5,7 @@ from auxiliar.auxiliar import *
 import plotly.express as px
 
 st.set_page_config(page_title="Pró-Corpo - Relatórios", page_icon="💎",layout="wide")
-header_1, spacer,header_2 = st.columns([1,0.5,0.5])
+header_1, spacer,header_2 = st.columns([1,0.5,0.5]) # Caceçalho
 
 with header_1:
   st.title("Relatórios de Mídia")
@@ -22,7 +22,7 @@ with header_2:
   )
   botao_gerar_relatorio = st.button("Atualizar Relatórios")
 
-if len(data_seletor) == 2:
+if len(data_seletor) == 2: # Garante que a data será uma range
 
   start_date = data_seletor[0].strftime('%Y-%m-%d')
   end_date = data_seletor[1].strftime('%Y-%m-%d')
@@ -32,22 +32,13 @@ else:
   start_date = data_seletor[0].strftime('%Y-%m-%d')
   end_date = start_date
 
-if 'funil_df' not in st.session_state:
+if 'funil_df' not in st.session_state: # Inicia o dataframe principal
   st.session_state['funil_df'] = criar_funil(start_date,end_date)
 
-
-if botao_gerar_relatorio:
+if botao_gerar_relatorio: # Atualiza o dataframe principal
   st.session_state['funil_df'] = criar_funil(start_date,end_date)
 
 funil_df = st.session_state['funil_df']
-
-st.title("Funil por Unidade")
-
-groupby_unidade = funil_df.groupby(['Unidade']).agg({'Leads':'sum','Agendamentos':'sum','Atendimentos':'sum','Receita':'sum','Vendas':'sum'}).reset_index()
-
-st.dataframe(groupby_unidade,hide_index = True,use_container_width=True)
-
-st.title("Funil por Data")
 
 opcoes_unidades = funil_df['Unidade'].unique().tolist()
 opcoes_unidades.insert(0,"Todas Unidades")
@@ -58,8 +49,6 @@ if filtro_unidade != "Todas Unidades":
   funil_df = funil_df.loc[funil_df['Unidade'] == filtro_unidade]
 
 groupby_data = funil_df.groupby(['Data']).agg({'Leads':'sum','Agendamentos':'sum','Atendimentos':'sum','Receita':'sum','Vendas':'sum'}).reset_index()
-
-st.dataframe(groupby_data,hide_index = True,use_container_width=True)
 
 st.title("Visualizar Evolução por dia")
 
@@ -81,3 +70,14 @@ fig = px.bar(
 )
 
 st.plotly_chart(fig, use_container_width=True)
+
+st.title("Funil por Unidade")
+
+groupby_unidade = funil_df.groupby(['Unidade']).agg({'Leads':'sum','Agendamentos':'sum','Atendimentos':'sum','Receita':'sum','Vendas':'sum'}).reset_index()
+
+st.dataframe(groupby_unidade,hide_index = True,use_container_width=True)
+
+st.title("Funil por Data")
+
+st.dataframe(groupby_data,hide_index = True,use_container_width=True)
+
